0a182e633df0df5d9dbcd8243f00fa45d32506d3
Damian Cop <cop.damian@gmail.com>
2020-06-17 08:56:05 +0200

    Add basic modules and folder structure:

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./modules/welcome/welcome.module').then(m => m.WelcomeModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/modules/authorization/authorization.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SignInComponent } from './pages/sign-in/sign-in.component';
+import { SignUpComponent } from './pages/sign-up/sign-up.component';
+
+
+
+@NgModule({
+  declarations: [SignInComponent, SignUpComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class AuthorizationModule { }
